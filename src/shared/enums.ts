383cab49fb9a5d884c2bc1a5a9dc999d0824d0b4
Alessia Amitrano <alessia.amitranobo@gmail.com>
2021-12-01 00:12:51 +0000

    build gh-pages

--- src/shared/enums.ts
+++ src/shared/enums.ts
@@ -1,5 +1,5 @@
 export enum ROUTES {
-  HOME = '/',
-  WEB_DEV = '/fewebdev',
-  MINI_PAINTING = '/minipainting'
+  HOME = '/alessia-website',
+  WEB_DEV = '/alessia-website/fewebdev',
+  MINI_PAINTING = '/alessia-website/minipainting',
 }
